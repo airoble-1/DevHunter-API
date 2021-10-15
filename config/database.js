cc4e819bc486059282823713fbf90deec4439fef
michael\Mike <mchong1123@gmail.com>
2021-10-15 00:40:37 -0400

    added comments and gitignore. Not much I can say about server architecture, it's a pretty simple CRUD platform

--- config/database.js
+++ config/database.js
@@ -7,6 +7,10 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
+        /*
+        for security reasons typically these will come from node environment variables.
+        the dev credentials should be stored with the project, prod credentials should be stored on the server
+        */
         database: env('DATABASE_NAME', 'communityone'),
         username: env('DATABASE_USERNAME', 'ahmed'),
         password: env('DATABASE_PASSWORD', 'ARules123@'),
