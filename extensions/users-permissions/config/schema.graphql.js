4eacbe1486762cc848c9bada01d92decef637634
Ahmed Roble <airoble2019@gmail.com>
2022-01-07 15:14:27 -0500

    Add current user query

---
+++ extensions/users-permissions/config/schema.graphql.js
@@ -0,0 +1,14 @@
+module.exports = {
+  query: `currentUser: UsersPermissionsUser!`,
+  resolver: {
+    Query: {
+      currentUser: {
+        description: "Return user",
+        resolverOf: "plugins::users-permissions.user.findOne",
+        resolver: async (obj, options, { context }) => {
+          return context.state.user;
+        },
+      },
+    },
+  },
+};
