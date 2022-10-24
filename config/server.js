eeab2dce062af736977592232af4dad6d4925b8b
airoble-1 <airoble2019@gmail.com>
2022-10-24 14:25:00 -0400

    Change to package-lock

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       // should probably come from node environment variable
-      secret: env('ADMIN_JWT_SECRET', '74f0ddd3b18aac6490c8eaf5dd6a9591'),
+      secret: env("ADMIN_JWT_SECRET", "74f0ddd3b18aac6490c8eaf5dd6a9591"),
     },
   },
 });
