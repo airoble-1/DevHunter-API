661b8f4369e960b4f40586a4b1a4cf749a23e5cd
airoble-1 <airoble2019@gmail.com>
2022-10-29 19:02:52 -0400

    Update nodemailer provider

--- config/database.js
+++ config/database.js
@@ -1,22 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
+        client: "postgres",
+        host: env("DATABASE_HOST", "containers-us-west-57.railway.app"),
+        port: env.int("DATABASE_PORT", 7369),
         /*
         for security reasons typically these will come from node environment variables.
         the dev credentials should be stored with the project, prod credentials should be stored on the server
         */
-        database: env('DATABASE_NAME', 'communityone'),
-        username: env('DATABASE_USERNAME', 'ahmed'),
-        password: env('DATABASE_PASSWORD', 'ARules123@'),
-        ssl: env.bool('DATABASE_SSL', false),
+        database: env("DATABASE_NAME", "railway"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "SkeF2ClIuQuWqQB31sLg"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {},
     },
   },
 });
